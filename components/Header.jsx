f282290add8fbff443e0ffddc222fde268d182ae
shm_usuf <shmusuf@gmail.com>
2023-08-12 21:22:22 +0600

    add instagram logo to the left side of the header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="">
       {/* LEFT */}
-      <div className="">
+      <div className="flex justify-between items-center max-w-6xl">
         <div className="w-24 h-24 relative hidden lg:inline-grid cursor-pointer">
           <Image
             src="https://logos-download.com/wp-content/uploads/2016/03/Instagram_Logo_2016.png"
@@ -13,6 +13,15 @@ const Header = () => {
             className="object-contain"
           />
         </div>
+        <div className="w-10 h-16 relative lg:hidden cursor-pointer">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/9/95/Instagram_logo_2022.svg"
+            alt="instagram image"
+            layout="fill"
+            className="object-contain"
+          />
+        </div>
+        <h2>Right side</h2>
       </div>
       {/* MIDDLE */}
       {/* RIGHT */}
