aea767f889231eaf40a10478c8a3fbca0878c662
shm_usuf <shmusuf@gmail.com>
2023-08-19 10:25:36 +0600

    modify the mini profile and story component to incluede the session

--- components/Stories.jsx
+++ components/Stories.jsx
@@ -2,11 +2,13 @@
 
 import minifaker from "minifaker";
 import "minifaker/locales/en";
-import { useEffect, useState } from "react";
 import Story from "./Story";
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
 
 const Stories = () => {
   const [storyUsers, setStoryUsers] = useState([]);
+  const { data: session } = useSession();
 
   useEffect(() => {
     const storyUsers = minifaker.array(20, (i) => ({
@@ -19,6 +21,13 @@ const Stories = () => {
 
   return (
     <div className="flex space-x-2 p-6 bg-white mt-5 border border-gray-200 overflow-x-scroll rounded-sm scrollbar-none">
+      {session && (
+        <Story
+          img={session?.user.image}
+          username={session?.user.name.split(" ").join("").toLocaleLowerCase()}
+          isUser="true"
+        />
+      )}
       {storyUsers.map((user) => (
         <Story key={user.id} username={user.username} img={user.img} />
       ))}
--- components/Story.jsx
+++ components/Story.jsx
@@ -1,13 +1,16 @@
-const Story = ({ username, img }) => {
+import { PlusIcon } from "@heroicons/react/solid";
+
+const Story = ({ username, img, isUser }) => {
   return (
-    <div>
+    <div className="relative group cursor-pointer">
       <picture className="">
         <img
           src={img}
           alt={username}
-          className="h-14 rounded-full p-[1.5px] border-2 border-rose-500 cursor-pointer ease-out hover:scale-110 transition-transform duration-200"
+          className="h-14 rounded-full p-[1.5px] border-2 border-rose-500 ease-out group-hover:scale-110 transition-transform duration-200"
         />
       </picture>
+      {isUser && <PlusIcon className="h-6 absolute top-4 left-4 text-white" />}
       <h3 className="text-xs w-14 truncate">{username}</h3>
     </div>
   );
